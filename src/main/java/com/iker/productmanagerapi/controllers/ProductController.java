2ef20f70b4a4c85a9bca14aa8340c0a1f780b787
Iker <162556194+IkerOwO@users.noreply.github.com>
2026-07-06 18:31:51 +0200

    Correcion para conexion

--- src/main/java/com/iker/productmanagerapi/controllers/ProductController.java
+++ src/main/java/com/iker/productmanagerapi/controllers/ProductController.java
@@ -7,6 +7,7 @@ import com.iker.productmanagerapi.services.ProductService;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/product")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ProductController {
     
     @Autowired
